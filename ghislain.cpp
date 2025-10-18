1cacd2156c8a6f77894b1fbfd56ba97dfd6ac62e
Ghislain KIMA <ghiskima@gmail.com>
2025-10-18 10:32:44 +0000

    Deuxième commite de Ghislain KIMA

--- ghislain.cpp
+++ ghislain.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include "ghislain.hpp"
+
 void presentation(std::string nom)
 {
     std::cout << "Je m'appelle : " << nom << std::endl;
---
+++ ghislain.hpp
@@ -0,0 +1,7 @@
+#ifndef GHISLAIN_HPP
+#define GHISLAIN_HPP
+
+#include <string>
+void presentation(std::string nom);
+
+#endif // GHISLAIN_HPP
